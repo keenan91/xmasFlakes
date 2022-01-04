f251d43b24b03969c926047ecf4f5f9822c35553
Keenan Matrix <keenanmatrixpt@gmail.com>
2022-01-04 17:42:47 -0500

    added webpack file

---
+++ webpack.common.js
@@ -0,0 +1,63 @@
+const Path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: Path.resolve(__dirname, '../src/scripts/index.js'),
+  },
+  output: {
+    path: Path.join(__dirname, '../build'),
+    filename: 'js/[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }]
+    }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/index.html'),
+    }),
+    new webpack.ProvidePlugin({ process: 'process/browser' }),
+    new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] })
+  ],
+  resolve: {
+    alias: {
+      '~': Path.resolve(__dirname, '../src'),
+    },
+    fallback: {
+      'crypto': false,
+      'stream': false,
+      'assert': false,
+      'http': false,
+      'https': false,
+      'os': false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      },
+    ],
+  },
+};
